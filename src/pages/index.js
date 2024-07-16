748d941a049969639ad74d78e28026b5fe1feffd
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2024-07-16 11:44:02 +0700

    ver. 2.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+"use client"
+
+import { useState, useEffect } from 'react';
 import FooterSection from "@/components/footer";
 import GearSection from "@/components/gear";
 import HeroParallax from "@/components/hero";
@@ -9,14 +12,38 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading for 3 seconds
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    // Clear timeout if component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <NavbarComponent/>
-      <HeroParallax/>
-      <HikingSection/>
-      <GearSection/>
-      <TimingSection/>
-      <FooterSection/>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <div className="relative">
+              <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+              <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-white animate-spin">
+              </div>
+          </div>
+      </div>
+      ) : (
+        <>
+          <NavbarComponent/>
+          <HeroParallax/>
+          <HikingSection/>
+          <GearSection/>
+          <TimingSection/>
+          <FooterSection/>
+        </>
+      )}
     </>
   );
 }
